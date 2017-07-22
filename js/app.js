ed17ea7b01617758846f8f173709a89e422d131a
Nikita <dev.potapy4@hotmail.com>
2017-07-22 11:46:12 +0300

    Added localization

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+	let initialLocaleCode = 'uk';
+
 	$('#calendar').fullCalendar({
 		defaultDate: '2017-07-17',
+		locale: initialLocaleCode,
 		navLinks: false, // can't click day/week names to navigate views
 		editable: true, // just for show cursor
 		events: eventData, // read from local JSON file
@@ -13,6 +16,23 @@ $(document).ready(function () {
 
 			$('.ui.modal')
 				.modal('show');
+		}
+	});
+
+	// build the locale selector's options
+	$.each($.fullCalendar.locales, function (localeCode) {
+		$('#locale-selector').append(
+			$('<option/>')
+			.attr('value', localeCode)
+			.prop('selected', localeCode == initialLocaleCode)
+			.text(localeCode)
+		);
+	});
+
+	// when the selected option changes, dynamically change the calendar option
+	$('#locale-selector').on('change', function () {
+		if (this.value) {
+			$('#calendar').fullCalendar('option', 'locale', this.value);
 		}
 	});
 
